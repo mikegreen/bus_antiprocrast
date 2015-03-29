56c318e5c03b7d3ef80e84d08866be2d29f03162
Mike Green <nekbet@gmail.com>
2015-03-29 01:11:11 +0000

    switch to pi_piper

--- led_reg.rb
+++ led_reg.rb
@@ -4,64 +4,28 @@
 
 require 'wiringpi'
 
-CLOCK_PIN = 4
-LATCH_PIN = 5
-DATA_PIN = 6 
-MR_PIN = 7
+clock_pin = 4
+latch_pin = 5
+data_pin = 6
 
 io = WiringPi::GPIO.new 
-# io.write(LATCH_PIN, LOW)
-# io.shiftOut(DATA_PIN, CLOCK_PIN,MSBFIRST,0)
-# io.write(LATCH_PIN, HIGH)
 
-io.mode(MR_PIN, OUTPUT)
-io.write(MR_PIN, LOW) # master reclear
-io.write(MR_PIN, HIGH) # enable again
+#io.mode(clock_pin, OUTPUT)
+#io.mode(latch_pin, OUTPUT)
+#io.mode(data_pin, OUTPUT)
 
-#io.mode(CLOCK_PIN, OUTPUT)
-#io.mode(LATCH_PIN, OUTPUT)
-#io.mode(DATA_PIN, OUTPUT)
-
-#io.shiftOutArray(DATA_PIN, CLOCK_PIN, LATCH_PIN,[0,0,0,0,0,0,0,0])
-
-sleep_time = 1.0/1.0
-
-puts "starting up"
-puts "all 1"
-io.shiftOutArray(DATA_PIN, CLOCK_PIN, LATCH_PIN, [1])
-io.shiftOutArray(DATA_PIN, CLOCK_PIN, LATCH_PIN, [1])
-io.shiftOutArray(DATA_PIN, CLOCK_PIN, LATCH_PIN, [1])
-io.shiftOutArray(DATA_PIN, CLOCK_PIN, LATCH_PIN, [0])
-sleep(sleep_time)
-io.shiftOutArray(DATA_PIN, CLOCK_PIN, LATCH_PIN, [1,1,1,1,1,1,1,1])
-io.shiftOutArray(DATA_PIN, CLOCK_PIN, LATCH_PIN, [1,1,1,1,1,1,1,1])
-io.shiftOutArray(DATA_PIN, CLOCK_PIN, LATCH_PIN, [1,1,1,1,1,1,1,1])
-io.shiftOutArray(DATA_PIN, CLOCK_PIN, LATCH_PIN, [1,1,1,1,1,1,1,1])
-io.shiftOutArray(DATA_PIN, CLOCK_PIN, LATCH_PIN, [1,1,1,1,1,1,1,1])
-io.shiftOutArray(DATA_PIN, CLOCK_PIN, LATCH_PIN, [1,1,1,1,1,1,1,1])
-sleep(sleep_time)
-puts "1/8"
-io.shiftOutArray(DATA_PIN, CLOCK_PIN, LATCH_PIN, [0,1,1,1,1,1,1,1])
-sleep(sleep_time)
-puts "2/8"
-io.shiftOutArray(DATA_PIN, CLOCK_PIN, LATCH_PIN, [0,0,1,1,1,1,1,1])
-sleep(sleep_time)
-puts "3/8"
-io.shiftOutArray(DATA_PIN, CLOCK_PIN, LATCH_PIN, [0,0,0,1,1,1,1,1])
-sleep(sleep_time)
-puts "4/8"
-io.shiftOutArray(DATA_PIN, CLOCK_PIN, LATCH_PIN, [0,0,0,0,1,1,1,1])
-sleep(sleep_time)
-puts "5/8"
-io.shiftOutArray(DATA_PIN, CLOCK_PIN, LATCH_PIN, [0,0,0,0,0,1,1,1])
-sleep(sleep_time)
-puts "6/8"
-io.shiftOutArray(DATA_PIN, CLOCK_PIN, LATCH_PIN, [0,0,0,0,0,0,1,1])
-sleep(sleep_time)
-puts "7/8"
-io.shiftOutArray(DATA_PIN, CLOCK_PIN, LATCH_PIN, [0,0,0,0,0,0,0,1])
-sleep(sleep_time)
-puts "8/8"
-io.shiftOutArray(DATA_PIN, CLOCK_PIN, LATCH_PIN, [0,0,0,0,0,0,0,0])
-
+sleep_time = 1.0/4.0
+puts "starting"
+io.shiftOutArray(data_pin, clock_pin, latch_pin, [0,0,0,0,0,0,0,0])
 sleep(1)
+io.shiftOutArray(data_pin, clock_pin, latch_pin, [1,1,1,1,1,1,1,1])
+sleep(1)
+io.shiftOutArray(data_pin, clock_pin, latch_pin, [0,0,0,0,0,0,1,1])
+#io.shiftOutArray(data_pin, clock_pin, latch_pin, [1,1,1,1,1,1,1,1])
+sleep(1)
+#io.write(clock_pin,0)
+#sleep(sleep_time)
+
+puts "clock_pin: #{io.read(clock_pin)}"
+puts "latch_pin: #{io.read(latch_pin)}"
+puts "data_pin: #{io.read(data_pin)}"
---
+++ pi_piper_ex.rb
@@ -0,0 +1,67 @@
+#!/usr/bin/env ruby
+# mike built this 2015-03-28
+
+require 'pi_piper'
+
+test_led_pin = 4
+
+sleep_time = 1.0/5.0
+
+puts "setup pins for pipiper"
+@mr_pin = PiPiper::Pin.new(pin: 18, direction: :out)
+test_led = PiPiper::Pin.new(pin: test_led_pin, direction: :out)
+@clock = PiPiper::Pin.new(pin: 23, direction: :out)
+@latch = PiPiper::Pin.new(pin: 24, direction: :out)
+@data = PiPiper::Pin.new(pin: 25, direction: :out)
+
+def clear_register
+	puts "master reset to start shift register over"
+	@mr_pin.off
+	@mr_pin.on
+	latch_go
+end
+
+def latch_go
+	@latch.on
+	@latch.off
+end
+
+clear_register
+
+puts "flash to say I'm alive"
+4.times do
+	puts "turn on LED"
+	test_led.on
+	sleep(sleep_time)
+	test_led.off
+	sleep(sleep_time)
+end
+
+def shift_bit
+	@data.on
+	@clock.on
+	@data.off
+	@clock.off
+end
+
+shift_bit
+latch_go
+sleep(sleep_time)
+shift_bit
+latch_go
+sleep(sleep_time)
+shift_bit
+latch_go
+sleep(sleep_time)
+shift_bit
+latch_go
+
+sleep(1)
+
+clear_register
+
+puts "done test"
+
+#puts "clock_pin: #{io.read(clock_pin)}"
+#puts "latch_pin: #{io.read(latch_pin)}"
+#puts "data_pin: #{io.read(data_pin)}"
